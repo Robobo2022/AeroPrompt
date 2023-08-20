ef1d281a4357121993322354405230e7133f4515
Bo <44844728+Robobo2022@users.noreply.github.com>
2023-08-21 01:44:03 +0200

    oki

--- Main/Core/install.sh
+++ Main/Core/install.sh
@@ -1,9 +1,24 @@
 #!/bin/bash
 
-git clone https://github.com/Robobo2022/Terminal.git ~/custom_scripts/RoboboTerminal
+REPO_URL="https://github.com/Robobo2022/AeroPrompt.git"
 
-chmod +x ~/custom_scripts/RoboboTerminal/Main/Core/Init.py
+REPO_DIR=~/custom_scripts/RoboboTerminal
+
+if [ -d "$REPO_DIR" ]; then
+    echo "Repository directory already exists. Removing existing repository..."
+    rm -rf "$REPO_DIR"
+fi
+
+git clone "$REPO_URL" "$REPO_DIR"
 
 echo 'export PATH="$HOME/custom_scripts/RoboboTerminal/Main/Core:$PATH"' >> ~/.bashrc
 
+source ~/.bashrc
+
+chmod +x ~/custom_scripts/RoboboTerminal/Main/Core/Init.py
+
+ln -s ~/custom_scripts/RoboboTerminal/Main/Core/Init.py ~/custom_scripts/RoboboTerminal/Main/Core/Init
+
 echo "Installation completed. You can now use the 'Init' terminal command."
+
+
---
+++ README.md
@@ -0,0 +1,7 @@
+# AeroPrompt
+
+The Terminal Multi-Tool, crafted entirely in Python, is your solution for streamlined system analysis and versatile command-line tasks.
+
+```sh
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Robobo2022/Terminal/main/Main/Core/install.sh)"
+```
