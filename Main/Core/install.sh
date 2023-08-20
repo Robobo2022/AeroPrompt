f00430898ba827c3d3eeba5493e4b3b2e0dbc4b1
Bo <44844728+Robobo2022@users.noreply.github.com>
2023-08-21 01:48:25 +0200

    oki

--- Main/Core/install.sh
+++ Main/Core/install.sh
@@ -2,7 +2,7 @@
 
 REPO_URL="https://github.com/Robobo2022/AeroPrompt.git"
 
-REPO_DIR=~/custom_scripts/RoboboTerminal
+REPO_DIR=~/AeroPrompt
 
 if [ -d "$REPO_DIR" ]; then
     echo "Repository directory already exists. Removing existing repository..."
@@ -11,13 +11,13 @@ fi
 
 git clone "$REPO_URL" "$REPO_DIR"
 
-echo 'export PATH="$HOME/custom_scripts/RoboboTerminal/Main/Core:$PATH"' >> ~/.bashrc
+echo 'export PATH="$HOME/AeroPrompt/Main/Core:$PATH"' >> ~/.bashrc
 
 source ~/.bashrc
 
-chmod +x ~/custom_scripts/RoboboTerminal/Main/Core/Init.py
+chmod +x ~/AeroPrompt/Main/Core/Init.py
 
-ln -s ~/custom_scripts/RoboboTerminal/Main/Core/Init.py ~/custom_scripts/RoboboTerminal/Main/Core/Init
+ln -s ~/AeroPrompt/Main/Core/Init.py ~/AeroPrompt/Main/Core/Init
 
 echo "Installation completed. You can now use the 'Init' terminal command."
 
--- README.md
+++ README.md
@@ -1,7 +1,8 @@
 # AeroPrompt
-
 The Terminal Multi-Tool, crafted entirely in Python, is your solution for streamlined system analysis and versatile command-line tasks.
 
+## Install AeroPrompt
+Paste that in a macOS Terminal or Linux shell prompt.
 ```sh
-/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Robobo2022/Terminal/main/Main/Core/install.sh)"
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Robobo2022/AeroPrompt/main/Main/Core/install.sh)"
 ```
