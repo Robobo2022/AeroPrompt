09ddff24dd454c9b6d2e4c6de34f49e6c5074a4e
Bo <44844728+Robobo2022@users.noreply.github.com>
2023-08-20 23:42:36 +0200

    Update install.sh

--- Main/Core/install.sh
+++ Main/Core/install.sh
@@ -1,6 +1,4 @@
 #!/bin/bash
-
-# Download the remote script
 wget -O Init.py https://raw.githubusercontent.com/Robobo2022/Terminal/main/Main/Core/Init.py
 
 chmod +x Init.py
@@ -8,8 +6,7 @@ chmod +x Init.py
 mkdir -p ~/custom_scripts
 mv Init.py ~/custom_scripts/
 
-# Add the directory to PATH
-echo "export PATH=\"$HOME/custom_scripts:\$PATH\"" >> ~/.bashrc
+echo 'export PATH="$HOME/custom_scripts:$PATH"' >> ~/.bashrc
 source ~/.bashrc
 
 echo "Installation completed. You can now use the 'Init.py' terminal command."
