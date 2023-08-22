0b360b5b829f6ee2592606ee72d79a4c06e6e6c5
Bo <44844728+Robobo2022@users.noreply.github.com>
2023-08-22 11:02:32 +0200

    Update install.sh

--- Main/Core/install.sh
+++ Main/Core/install.sh
@@ -1,22 +1,21 @@
 #!/bin/bash
 
 REPO_URL="https://github.com/Robobo2022/AeroPrompt.git"
-REPO_DIR=~/AeroPrompt
+REPO_DIR="$HOME/AeroPrompt"
 
 if [ -d "$REPO_DIR" ]; then
     echo "Repository directory already exists. Removing existing repository..."
     rm -rf "$REPO_DIR"
 fi
-
 git clone "$REPO_URL" "$REPO_DIR"
 
 echo 'export PATH="$HOME/AeroPrompt/Main/Core:$PATH"' >> ~/.bashrc
 
 source ~/.bashrc
 
-chmod +x ~/AeroPrompt/Main/Core/Init.py
+chmod +x "$REPO_DIR/Main/Core/Init.py"
 
-ln -s ~/AeroPrompt/Main/Core/Init.py ~/AeroPrompt/Main/Core/AeroPrompt
+ln -s "$REPO_DIR/Main/Core/Init.py" "$REPO_DIR/Main/Core/AeroPrompt"
 
 REQUIREMENTS_FILE="$REPO_DIR/Main/Core/requirements.txt"
 if [ -f "$REQUIREMENTS_FILE" ]; then
